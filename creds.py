19f5ea80b115be00fb847e29e87aa315493cbf6f
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-05-30 04:14:30 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "5379457155:AAFsXnVKD0GK4SFRSXeEXyBPQuC2vsUwB5Q"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1hY0U9oCtxv_rtF6F97W52i1XX1ovnVTR"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AD1WUTXehPTZUk9PVA"
     
     
     
